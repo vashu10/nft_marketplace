0aab505e3611c24c6f3ac67e54a36ba56ae11cfd
Vashu Sinha <vashusinha07@gmail.com>
2022-10-29 00:13:58 +0530

    best sellers completed

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,15 @@ const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div className="dark:bg-nft-dark bg-white min-h-screen">
       <Navbar />
-      <Component {...pageProps} />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
       <Footer />
     </div>
-    <Script src="https://kit.fontawesome.com/17222953e3.js" crossOrigin="anonymous" />
+    <Script
+      src="https://kit.fontawesome.com/17222953e3.js"
+      crossOrigin="anonymous"
+    />
   </ThemeProvider>
 );
 
