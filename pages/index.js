3e242119dd1e1dbd35d2f05be27f6ddfaabeaa8d
Vashu Sinha <vashusinha07@gmail.com>
2022-10-31 00:40:16 +0530

    Create NFT page is completed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 
 import images from '../assets';
 import { makeId } from '../utils/makeId';
@@ -28,11 +28,8 @@ const Home = () => {
     const { current } = scrollRef;
     const { current: parent } = parentRef;
 
-    if (current?.scrollWidth >= parent?.offsetWidth) {
-      setHideButtons(false);
-    } else {
-      setHideButtons(true);
-    }
+    if (current?.scrollWidth >= parent?.offsetWidth) return setHideButtons(false);
+    return setHideButtons(true);
   };
 
   useEffect(() => {
@@ -48,7 +45,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name="Discover, collect, and sell extraordinay NFTs"
+          name="Discover, collect, and sell extraordinary NFTs"
           parentStyles="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
         />
@@ -71,34 +68,59 @@ const Home = () => {
                 />
               ))}
               {!hideButtons && (
-              <>
-                <div
-                  onClick={() => handleScroll('left')}
-                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
-                >
-                  <Image
-                    src={images.left}
-                    layout="fill"
-                    objectFit="contain"
-                    alt="left_arrow"
-                    className={theme === 'light' && 'filter invert'}
-                  />
-                </div>
-                <div
-                  onClick={() => handleScroll('right')}
-                  className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
-                >
-                  <Image
-                    src={images.right}
-                    layout="fill"
-                    objectFit="contain"
-                    alt="right_arrow"
-                    className={theme === 'light' && 'filter invert'}
-                  />
-                </div>
-              </>
+                <>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                  >
+                    <Image
+                      src={images.left}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                  >
+                    <Image
+                      src={images.right}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="right_arrow"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                </>
               )}
             </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mg-4">
+              Hot Bids
+            </h1>
+            <div>
+              Searchbar
+            </div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
